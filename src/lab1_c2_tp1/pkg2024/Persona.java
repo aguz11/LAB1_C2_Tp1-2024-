7776a6d949ed03da12e1b41d14f663309738c8a3
aguz11 <agus110196@gmail.com>
2024-03-19 15:49:01 -0300

    Atributos/Getters/Setter
    Se agregaron atributos y sus respectivos getters y setters a la clase Persona.

--- src/lab1_c2_tp1/pkg2024/Persona.java
+++ src/lab1_c2_tp1/pkg2024/Persona.java
@@ -2,6 +2,36 @@
 package lab1_c2_tp1.pkg2024;
 
 public class Persona {
+    private String nombre;
+    private int edad;
+    private double altura;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public double getAltura() {
+        return altura;
+    }
+
+    public void setAltura(double altura) {
+        this.altura = altura;
+    }
+    
+    
+    
     
     public static void Saludar(){
        System.out.println("Hola");
@@ -11,5 +41,7 @@ public class Persona {
         System.out.println("Chau");
     
     }
+    
+    
     
 }
