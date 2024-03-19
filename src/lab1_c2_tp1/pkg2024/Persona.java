1cc4f787f77595600bf8c446f269507b4ec1937c
aguz11 <agus110196@gmail.com>
2024-03-19 16:58:49 -0300

    Constructor Persona
    Se agrego el constructor con parámetros para la clase Persona.

--- src/lab1_c2_tp1/pkg2024/Persona.java
+++ src/lab1_c2_tp1/pkg2024/Persona.java
@@ -6,6 +6,13 @@ public class Persona {
     private int edad;
     private double altura;
 
+    public Persona(String nombre, int edad, double altura) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.altura = altura;
+    }
+
+
     public String getNombre() {
         return nombre;
     }
