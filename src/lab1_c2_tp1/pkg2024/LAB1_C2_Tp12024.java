bf34e622ad6a32ddf90e2c66e141616f43b30f04
aguz11 <agus110196@gmail.com>
2024-03-19 15:43:44 -0300

    Initial commit

---
+++ src/lab1_c2_tp1/pkg2024/LAB1_C2_Tp12024.java
@@ -0,0 +1,17 @@
+/*
+
+ */
+package lab1_c2_tp1.pkg2024;
+
+//Agustin Jofre
+
+public class LAB1_C2_Tp12024 {
+
+    public static void main(String[] args) {
+    
+            Persona.Saludar();
+            System.out.println("\n");
+            Persona.Despedirse();
+    }
+    
+}
---
+++ src/lab1_c2_tp1/pkg2024/Persona.java
@@ -0,0 +1,15 @@
+
+package lab1_c2_tp1.pkg2024;
+
+public class Persona {
+    
+    public static void Saludar(){
+       System.out.println("Hola");
+    }
+    
+    public static void Despedirse(){
+        System.out.println("Chau");
+    
+    }
+    
+}
